d720a7a695ad2cc6e8eb3a7a169f011761b895d4
hyeonji0409 <guswl8507@naver.com>
2022-01-07 11:32:29 +0900

    spring annotation

---
+++ spring_mvc01/src/main/java/com/spring_mvc/project/Product.java
@@ -0,0 +1,46 @@
+package com.spring_mvc.project;
+
+public class Product {
+	String no;
+	String name;
+	String company;
+	String price;
+	String date;
+	String qty;
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getQty() {
+		return qty;
+	}
+	public void setQty(String qty) {
+		this.qty = qty;
+	}
+	public String getdate() {
+		return date;
+	}
+	public void setdate(String date) {
+		date = date;
+	}
+}
